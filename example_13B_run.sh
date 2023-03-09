c31aae6cb5dd15c2a38709c13c19a76ea387f028
Brian Wu <brianwu@cs.stanford.edu>
2023-03-09 18:18:15 -0500

    Example inference scripts for 7B and 13B models

---
+++ example_13B_run.sh
@@ -0,0 +1,1 @@
+torchrun --nproc_per_node 2 example.py --ckpt_dir /bigdrive/llama_weights/13B --tokenizer_path /bigdrive/llama_weights/tokenizer.model
---
+++ example_7B_run.sh
@@ -0,0 +1,1 @@
+torchrun --nproc_per_node 1 example.py --ckpt_dir /bigdrive/llama_weights/7B --tokenizer_path /bigdrive/llama_weights/tokenizer.model
